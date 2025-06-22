6a9be4af2fb49c0e2f3229a47862e3f268cf77ad
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-06-22 17:13:21 +0300

    доработка splice

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -87,7 +87,8 @@ class Library {
 	giveBookByName(bookName) {
 		const giveBook = this.books.find(book => book.name === bookName);
 		if (giveBook) {
-			this.books.splice(giveBook);
+			const index = this.books.indexOf(giveBook);
+			this.books.splice(index, 1);
 			return giveBook;
 		} else {
 			return null;
@@ -95,68 +96,5 @@ class Library {
 	}
 }
 
-function Student(name, gender, age) {
-	this.name = name;
-	this.gender = gender;
-	this.age = age;
-	this.marks = [];
-}
 
-/*student1 = new Student("Igor", "man", 18);
-student2 = new Student("Olga", "woman", 19);
-student3 = new Student("Petr", "man", 22);
-*/
 
-Student.prototype.setSubject = function(subjectName) {
-	this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function(...marks) {
-	if (this.marks) {
-		this.marks.push(...marks);
-	}
-}
-
-Student.prototype.getAverage = function() {
-	if (!this.marks || this.marks.length === 0) {
-		return 0;
-	}
-	let sum = this.marks.reduce((accumulator, mark) => accumulator + mark, 0)
-	return sum / this.marks.length;
-}
-
-Student.prototype.exclude = function(reason) {
-	delete this.subject;
-	delete this.marks;
-	this.excluded = reason;
-}
-
-class Student1 {
-	constructor(name, allMarks = {}) {
-		this.name = name;
-		this.allMarks = allMarks;
-	}
-
-	addMark(value, subject) {
-		if (value >= 2 && value <= 5) {
-			if (!this.allMarks[subject]) {
-				this.allMarks[subject] = [];
-			}
-			this.allMarks[subject].push(value);
-		}
-	}
-
-	getAverageBySubject(subject) {
-		if (!this.allMarks[subject]) {
-			return 0;
-		}
-
-		const summOfMarks = this.allMarks[subject].reduce((acc, item) => acc + item, 0);
-		return summOfMarks / this.allMarks[subject].length;
-	}
-
-	getAverage() {
-		Object.keys
-		reduce
-	}
-}
