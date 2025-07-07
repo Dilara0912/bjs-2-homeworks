080f00d372fcec7a6ff92f9f6b3611657ebf16d3
Dilara Gilfanova <dgilfanova12@gmail.com>
2025-07-07 12:37:35 +0300

    Седьмое задание готово, но 1 тест не прошел

---
+++ 7.async/task.js
@@ -0,0 +1,68 @@
+class AlarmClock {
+	constructor(alarmCollection = [], intervalId = null) {
+		this.alarmCollection = alarmCollection;
+		this.intervalId = intervalId;
+	}
+
+	addClock(targetTime, callback) {
+		if (!targetTime || typeof callback !== 'function') {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
+
+		const hasCallAtSameTime = this.alarmCollection.some(alarm => alarm.time === targetTime);
+		if (hasCallAtSameTime) {
+			console.warn('Уже присутствует звонок на это же время');
+			return;
+		}
+
+		const newCall = {
+			callback: callback,
+			time: targetTime,
+			canCall: true
+		}
+
+		this.alarmCollection.push(newCall);
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		const now = new Date();
+		const currentTime = now.toTimeString().slice(0, 5);
+		return currentTime;
+	}
+
+	start() {
+		if (this.intervalId) {
+			return;
+		}
+
+		this.intervalId = setInterval(() => {
+			const currentTime = this.getCurrentFormattedTime();
+
+			this.alarmCollection.forEach(alarm => {
+				if (alarm.time === currentTime && alarm.canCall === true) {
+					alarm.canCall = false;
+					alarm.callback();
+				}
+			});
+		}, 1000);
+
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(alarm => alarm.canCall = true);
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = []
+	}
+}
